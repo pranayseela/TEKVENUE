6cc36850289cbc2a5cb8268cdcc0ddd1862a5a2c
pranay seela <pranayritvik@gmail.com>
2017-10-08 20:23:20 -0500

    updated contect and design

--- js/google_map.js
+++ js/google_map.js
@@ -5,7 +5,8 @@ function init() {
     // Basic options for a simple Google Map
     // For more options see: https://developers.google.com/maps/documentation/javascript/reference#MapOptions
     // var myLatlng = new google.maps.LatLng(40.71751, -73.990922);
-    var myLatlng = new google.maps.LatLng(40.69847032728747, -73.9514422416687);
+    var myLatlng = new google.maps.LatLng(33.095146, -96.781215);
+    //33.095146, -96.781215
     // 39.399872
     // -8.224454
     
